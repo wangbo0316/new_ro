710223a375faa2f2794520ced9aeb81fd202c978
wangbo0316 <wangbo@lcservis.com>
2019-01-29 11:07:21 +0800

    '1'

---
+++ config/setting.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+
+MAIN_PATH = os.getcwd()
+STATIC_PATH = MAIN_PATH + '\\static'
+sys.path.append(MAIN_PATH + '\\script')
+sys.path.append(MAIN_PATH + '\\static')
+sys.path.append(MAIN_PATH + '\\ui')
